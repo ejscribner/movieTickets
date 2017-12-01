fe08d9b2b83e5908a8a1f682bc3c79a92060dc07
ejscribner <ejscribner@gmail.com>
2017-11-30 21:04:50 -0500

    initial project setup

---
+++ src/MovieTickets.java
@@ -0,0 +1,11 @@
+/*
+    Created By: Elliot J Scribner on 11/30/17
+    Student ID: ejs320
+    Lab #: **Num**
+    MovieTickets: **Description**
+ */
+
+package PACKAGE_NAME;
+
+public class MovieTickets {
+}
