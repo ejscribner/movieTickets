310b05230b3de7b81ddfe9b7504b2941834068f5
ejscribner <ejscribner@gmail.com>
2017-12-06 20:17:30 -0500

    added admin panel and ticket sales calculations

--- src/MovieTickets.java
+++ src/MovieTickets.java
@@ -5,6 +5,7 @@
     MovieTickets: Machine to purchase movie tickets
  */
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
@@ -16,23 +17,30 @@ public class MovieTickets {
         Scanner input = new Scanner(System.in);
         System.out.println("Powering On");
         Boolean loginToken = adminLogin("admin", "123", input);
-        //Manager enters data
         String movieName = null;
         double ticketPrice = 0.0;
+        int seatCount = 50;
         if(loginToken) {
             movieName = getMovieName(input);
             ticketPrice = getTicketPrice(input);
-            System.out.println("Manager setup complete! To access the manager dashboard, press '2' after seeing the welcome message at any time");
+            System.out.println("Manager setup complete!");
+            lineBreak();
         }
         do {
-            customerInteraction(movieName, ticketPrice, input);
+            int seatsSold = customerInteraction(movieName, ticketPrice, seatCount, input);
+            seatCount -= seatsSold;
         } while (true);
     }
-    public static void customerInteraction(String movieName, double ticketPrice, Scanner input) {
-        customerDisplay(movieName, ticketPrice, input);
+    public static void lineBreak() {
+        System.out.println("-----------------------------------");
+    }
+    public static int customerInteraction(String movieName, double ticketPrice, int seatCount, Scanner input) {
+        customerDisplay(movieName, ticketPrice, seatCount, input);
         int numTix = customerTicketNumber(input);
         double change = cashOut(numTix, ticketPrice, input);
         System.out.println("Your change is: " + change);
+        lineBreak();
+        return numTix;
     }
 
     public static String getMovieName(Scanner input) {
@@ -47,23 +55,44 @@ public class MovieTickets {
         return ticketPrice;
     }
 
-    public static void customerDisplay (String movieName, double ticketPrice, Scanner input) {
+    public static void customerDisplay (String movieName, double ticketPrice, int seatCount, Scanner input) {
         System.out.println("Welcome to the theater! Press '1' to purchase tickets or '2' to access the manager dashboard");
         int purchaseAuth = getInput(6, input);
         if (purchaseAuth == 2) {
-            System.out.println("Please enter your username and password to power off the machine");
-            Boolean logoutToken = adminLogout("admin", "123", movieName, ticketPrice, input);
+            lineBreak();
+            System.out.println("Please log in to view dashboard");
+            Boolean logoutToken = adminLogout("admin", "123", movieName, ticketPrice, seatCount, input);
             if(logoutToken) {
-                System.exit(0);
+                mgrDash(movieName, ticketPrice, seatCount, input);
             }
         }if (purchaseAuth == 1) {
-            System.out.println("5");
             System.out.println("Movie Name: " + movieName);
             System.out.println("Showtime: " + 6);
             System.out.println("Price: $" + ticketPrice);
         } else {
             System.out.println("Invalid entry");
-            customerDisplay(movieName, ticketPrice, input);
+            customerDisplay(movieName, ticketPrice, seatCount, input);
+        }
+    }
+    public static void mgrDash(String movieName, double ticketPrice, int seatCount, Scanner input) {
+        int seatsRemaining = 50-seatCount;
+        double cashMade = seatsRemaining*ticketPrice;
+        System.out.println("Manager Dashboard");
+        System.out.println("-------------------");
+        System.out.println("Today's Movie: " + movieName);
+        System.out.println("Ticket Price: " + ticketPrice);
+        System.out.println("Tickets sold: " + seatCount);
+        System.out.println("Money earned: " + cashMade);
+        System.out.println("Press 1 to power off the machine, or 2 to return to customer mode");
+        int offToken = getInput(1, input);
+        if(offToken == 1) {
+            System.out.println("Powering Down");
+            System.exit(0);
+        } else if(offToken == 2) {
+            customerDisplay(movieName, ticketPrice, seatCount, input);
+        } else {
+            System.out.println("Invalid entry");
+            mgrDash(movieName, ticketPrice, seatCount, input);
         }
     }
 
@@ -98,30 +127,34 @@ public class MovieTickets {
         if(enteredUsername.equals(username) && enteredPassword.equals(password)) {
             hasPasswordToken = true;
             System.out.println("Login Successful!");
+            lineBreak();
         }
         if(!hasPasswordToken){
             System.out.println("Incorrect Username or Password, please try again!");
+            lineBreak();
             hasPasswordToken = adminLogin(username, password, input);
         }
         return hasPasswordToken;
     }
 
-    public static Boolean adminLogout(String username, String password, String movieName, double ticketPrice, Scanner input) {
+    public static Boolean adminLogout(String username, String password, String movieName, double ticketPrice, int seatCount, Scanner input) {
+        getInput("", input);
         System.out.print("Please type your username: ");
         String enteredUsername = getInput("", input);
-        if(enteredUsername.equalsIgnoreCase("p")) {
-            customerInteraction(movieName, ticketPrice, input);
+        if(enteredUsername.equalsIgnoreCase("1")) {
+            lineBreak();
+            customerInteraction(movieName, ticketPrice, seatCount, input);
         }
         System.out.print("Please type your password: ");
         String enteredPassword = getInput("", input);
         Boolean hasPasswordToken = false;
         if(enteredUsername.equals(username) && enteredPassword.equals(password)) {
             hasPasswordToken = true;
-            System.out.println("Powering Down");
         }
         if(!hasPasswordToken){
-            System.out.println("Incorrect Username or Password, please try again or press 'p' to purchase tickets");
-            hasPasswordToken = adminLogout(username, password, movieName, ticketPrice, input);
+            System.out.println("Incorrect Username or Password, returning to customer mode");
+            lineBreak();
+            customerInteraction(movieName, ticketPrice, seatCount, input);
         }
         return hasPasswordToken;
     }
